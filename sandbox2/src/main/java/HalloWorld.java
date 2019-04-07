02c61d93ce8bbd9e4578bae5522fb54ba40584bc
Ma3dai <mvgrabov@gmail.com>
2019-04-07 22:57:46 +0300

    Создан пакет для проги

--- sandbox2/src/main/java/HalloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HalloWorld {
-
-    public static void main (String[] args) {
-
-        System.out.println("hi");
-    }
-}
---
+++ sandbox2/src/main/java/ru/stqa/sandbox2/Hallo1.java
@@ -0,0 +1,8 @@
+package ru.stqa.sandbox2;
+
+public class Hallo1 {
+
+    public static void main (String[] args) {
+        System.out.println("Hi");
+    }
+}
