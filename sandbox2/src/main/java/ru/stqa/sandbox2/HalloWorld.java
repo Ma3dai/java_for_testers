5b7bb800932808106137dbd10a229d504c6c20ae
Mikhail <mvgrabov@gmail.com>
2019-04-11 12:38:58 +0300

    раскидал методы по классам

--- sandbox2/src/main/java/ru/stqa/sandbox2/HalloWorld.java
+++ sandbox2/src/main/java/ru/stqa/sandbox2/HalloWorld.java
@@ -9,21 +9,17 @@ public class HalloWorld {
 
 
         Square s = new Square(30);
-        System.out.println("Площадь квадрата со сторонами " + s.l + " равна " + area(s));
+        System.out.println("Площадь квадрата со сторонами " + s.l + " равна " + s.area());
 
         Rectangle r = new Rectangle(4, 7);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
     }
 
     public static void hallo(String somebody) {
         System.out.println("Hallo" + somebody);
     }
 
-    public static double area (Square s){
-        return s.l * s.l;
-    }
 
-    public static double area (Rectangle r){
-        return r.a * r.b;
-    }
+
+
 }
